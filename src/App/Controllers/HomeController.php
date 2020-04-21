0ca4b571c1a714547c10f7b0748d41abbff222a1
azharisaputrasikumbang@gmail.com <azharisaputrasikumbang@gmail.com>
2020-04-22 00:31:25 +0700

    Create Sample Controller

---
+++ src/App/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+namespace Myu\App\Controllers;
+
+use Myu\Components\Routing\Controller;
+
+/**
+ * Home Controller
+ */
+class HomeController extends Controller
+{
+	public function index()
+	{
+		echo "home page";
+	}
+}
