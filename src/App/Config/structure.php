495a3eeedccd754ced7c7337c5db49517f351f5c
azharisaputrasikumbang@gmail.com <azharisaputrasikumbang@gmail.com>
2020-04-22 00:31:09 +0700

    Create sample config file

---
+++ src/App/Config/structure.php
@@ -0,0 +1,8 @@
+<?php
+return [
+	"controllers_folder" => "Controller",
+	"views_folder" => "Views",
+	"models_folder" => "Models",
+	"helpers_folder" => "Helpers",
+	"assets_folder" => "assets"
+];
