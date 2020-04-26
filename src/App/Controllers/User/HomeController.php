30236e47b589c6c0aa3ba9803f6f1e31c389db9e
azharisaputrasikumbang@gmail.com <azharisaputrasikumbang@gmail.com>
2020-04-26 17:18:48 +0700

    just test for controller on folder

---
+++ src/App/Controllers/User/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Myu\App\Controllers\User;
+
+use Myu\Components\Routing\Controller;
+
+/**
+ * User Home Conroller
+ */
+class HomeController extends Controller
+{
+	
+	public function index()
+	{
+		echo "user home file";
+	}
+}
---
+++ src/App/Controllers/User/Seller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+namespace Myu\App\Controllers\User\Seller;
+
+use Myu\Components\Routing\Controller;
+
+/**
+ * User Home Conroller
+ */
+class HomeController extends Controller
+{
+	public function index()
+	{
+	}
+}
