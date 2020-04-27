95f490b43b84bfa2345afb0044395a8a74e0c134
azharisaputrasikumbang@gmail.com <azharisaputrasikumbang@gmail.com>
2020-04-27 14:40:46 +0700

    add code comment

--- src/Bootstrap/Environment.php
+++ src/Bootstrap/Environment.php
@@ -5,18 +5,23 @@ use Dotenv\Dotenv;
 use Dotenv\Exception\InvalidFileException;
 
 /**
- * Load Environment 
+ * Environment Handler 
  */
 class Environment
 {
-	
+	/**
+	* This static function will be load a environtment file
+	* @param string @path for file path
+	* @return void; 
+	*/
 	public static function load($path)
 	{
+		// Try catch for environment file
 		try {
 			return Dotenv::createImmutable($path)->load();
 		} catch (Exception $e) {
-			echo "Error " . $e->getMessage();
-			die;
+			http_response_code(500);
+			throw new \Myu\Handler\Error("Your environtment files doesnt exitst", 500);
 		}
 	}
 }
